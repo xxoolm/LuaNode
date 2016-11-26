6722ddb79c4bf120ab0a647271f0341fcfb0a2af
Nicholas3388 <tulip-wangwei@163.com>
2016-11-26 12:43:55 +0800

    Change directory

--- LuaNode_Esp32/LuaNode32/components/spiffs/include/spiffs_config.h
+++ LuaNode_Esp32/LuaNode32/components/spiffs/include/spiffs_config.h
@@ -8,9 +8,28 @@
 #ifndef SPIFFS_CONFIG_H_
 #define SPIFFS_CONFIG_H_
 
-#include "nodemcu_spiffs.h"
 #include "sdkconfig.h"
 #include "c_types.h"
+
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdio.h>
+
+// Turn off stats
+#define SPIFFS_CACHE_STATS 	    0
+#define SPIFFS_GC_STATS             0
+
+// Needs to align stuff
+#define SPIFFS_ALIGNED_OBJECT_INDEX_TABLES	1
+
+// Enable magic so we can find the file system (but not yet)
+#define SPIFFS_USE_MAGIC            1
+#define SPIFFS_USE_MAGIC_LENGTH     1
+
+// Reduce the chance of returning disk full
+#define SPIFFS_GC_MAX_RUNS          256
+
 
 // compile time switches
 
